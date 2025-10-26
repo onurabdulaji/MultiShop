456842504ee9c901e53d7a44fbfa650a109ee804
onura <onurabdulaji@gmail.com>
2025-10-26 02:01:28 +0100

    Mongo Db Appsettings duzenlemeleri

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImage
+    {
+        public string ProductImageId { get; set; }
+        public string ProductImage1 { get; set; }
+        public string ProductImage2 { get; set; }
+        public string ProductImage3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
