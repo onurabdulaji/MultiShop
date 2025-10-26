0151e1bf067105a3d971a94f7773234aff0300e9
onura <onurabdulaji@gmail.com>
2025-10-26 02:20:12 +0100

    Some fixes in entities and Product Details , Product Images Dtos Created

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/CreateProductDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class CreateProductDetailsDto
+    {
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        public string ProductId { get; set; }
+    }
+}
