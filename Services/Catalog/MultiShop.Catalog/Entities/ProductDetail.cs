eb7ee030f9b537f2a7d1e35abf9004542d488f76
onura <onurabdulaji@gmail.com>
2025-10-26 02:58:25 +0200

    Catalog Servisinde Entitiler Eklendi

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -2,5 +2,8 @@
 {
     public class ProductDetail
     {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInformation { get; set; }
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string ProductImagesId { get; set; }
+        public string ProductImages1 { get; set; }
+        public string ProductImages2 { get; set; }
+        public string ProductImages3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
