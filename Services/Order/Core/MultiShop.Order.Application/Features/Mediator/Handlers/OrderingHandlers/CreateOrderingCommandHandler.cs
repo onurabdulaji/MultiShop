1538716a173056a2397765fb2dc7d2df2355bdd0
oabdulaji <oabdulaji@tessa.group>
2025-11-05 19:49:36 +0100

    MediatR Design Pattern , Ordering Implementation , Ordering Endpoints

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/CreateOrderingCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class CreateOrderingCommandHandler
+        (IRepository<Ordering> repository)
+        : IRequestHandler<CreateOrderingCommand>
+    {
+        public async Task Handle(CreateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await repository.CreateAsync(new Ordering
+            {
+                OrderDate = request.OrderDate,
+                TotalPrice = request.TotalPrice,
+                UserId = request.UserId,
+            });
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/GetOrderingByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingByIdQueryHandler
+        (IRepository<Ordering> repository)
+        : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        public async Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await repository.GetByIdAsync(request.Id);
+
+            return new GetOrderingByIdQueryResult
+            {
+                OrderDate = values.OrderDate,
+                OrderingId = values.OrderingId,
+                TotalPrice = values.TotalPrice,
+                UserId = values.UserId,
+            };
+        }
+    }
+}
