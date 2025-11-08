38cf365619fbbb1f2a12b003d1053257de8df2cd
oabdulaji <oabdulaji@tessa.group>
2025-11-08 03:30:27 +0100

    Admin UI Part - 1

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadMComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeadMComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSiderbarComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSiderbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
