91ce791a0d7ee05636b9c1834b424ccb68b042b8
oabdulaji <oabdulaji@tessa.group>
2025-11-08 02:48:55 +0100

    UI - Part 2

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewCompnents/_ShoppingCartDiscountComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewCompnents
+{
+    public class _ShoppingCartDiscountComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewCompnents/_ShoppingCartProductListComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewCompnents
+{
+    public class _ShoppingCartProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
