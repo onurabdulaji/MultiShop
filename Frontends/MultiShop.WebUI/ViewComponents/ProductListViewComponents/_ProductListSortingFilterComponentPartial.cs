7344f049cc2f7b10b3bc2ac901f3b9c74bfc89d2
oabdulaji <oabdulaji@tessa.group>
2025-11-08 01:59:30 +0100

    UI Part - 1

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListSortingFilterComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListSortingFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
